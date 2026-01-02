6191fe0d42807a3653b6f91331800e8a9506fe28
Mackay Grange <mackay.grange@gmail.com>
2026-01-01 17:02:38 -0700

    Fixes to deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: './src',
+  base: '/PersonalSite/',
   build: {
-    outDir: './dist',
+    outDir: '../dist',
+    emptyOutDir: true,
   },
 });
