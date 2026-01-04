eff92e51a64bf2ef9dcd86e2512e84c05f38c0ee
Mackay Grange <mackay.grange@gmail.com>
2026-01-04 09:55:42 -0700

    Small tweaks to home page in preparation for larger changes.

--- src/ts/pages/Home.tsx
+++ src/ts/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Tooltip } from 'react-tooltip';
+import { FaLightbulb } from 'react-icons/fa';
 import { SiC, SiCplusplus, SiGnubash, SiGooglesheets, SiTypescript, SiLua, SiBlender, SiGimp, SiFigma, SiArduino, SiLatex, SiCmake, SiMake, SiRos, SiLucid, SiUml, SiEspressif, SiArchlinux, SiUbuntu, SiHyprland, SiSqlite  } from 'react-icons/si';
 import { DiRasberryPi, DiJsBadge, DiNodejs, DiNpm, DiPython, DiReact, DiMysql, DiLinux, DiHtml5, DiGit, DiGithubBadge, DiDocker, DiCss3 } from "react-icons/di";
 import { IconWithTooltip } from '../components/IconWithTooltip';
 
 import profilePic  from '../../assets/images/PFP.jpg';
 import { Link } from 'react-router-dom';
+import { ContentCard } from '../components/ContentCard';
 
 export function Home()
 {
@@ -62,6 +64,13 @@ export function Home()
         <h2 className="text-3xl font-bold mb-6">Programming Languages</h2>
         <div className="border-b-4 border-double border-(--color-overlay) mb-4 max-w-2xl"> </div>
 
+        <div className="text-xs sm:text-sm text-(--color-subtle) p-2 sm:p-3 rounded mb-3 max-w-2xl">
+          <span className="flex items-start gap-2 ">
+            <FaLightbulb className="text-(--color-gold) mt-0.5 shrink-0" />
+            <span>Hover or tap an icon if you do not recognize it to see what it represents.</span>
+          </span>
+        </div>
+
         <div className="flex flex-wrap gap-4 max-w-2xl">
           <IconWithTooltip id="TT-C" icon={SiC} tooltipText="C Programming Language" />
           <IconWithTooltip id="TT-CPP" icon={SiCplusplus} tooltipText="C++ Programming Language" />
@@ -76,47 +85,47 @@ export function Home()
       <section id="skills" className="pb-16">
         <h2 className="text-3xl font-bold mb-6">Skills</h2>
         <div className="border-b-4 border-double border-(--color-overlay) mb-4 max-w-2xl"> </div>
+        <div className="flex flex-wrap gap-6 max-w-2xl">
+            <div className="flex flex-wrap gap-4 max-w-2xl rounded-2xl border-2 border-(--color-gold) p-2 bg-(--color-overlay)/50 mb-4" >
+                <IconWithTooltip id="TT-Linux" icon={DiLinux} tooltipText="Linux Operating System" bgColor="bg-(--color-gold)/50" />
+                <IconWithTooltip id="TT-Ubuntu" icon={SiUbuntu} tooltipText="Ubuntu Operating System" bgColor="bg-(--color-gold)/50" />
+                <IconWithTooltip id="TT-ArchLinux" icon={SiArchlinux} tooltipText="Arch Linux Operating System" bgColor="bg-(--color-gold)/50" />
+                <IconWithTooltip id="TT-Git" icon={DiGit} tooltipText="Git Version Control" bgColor="bg-(--color-gold)/50" />
+                <IconWithTooltip id="TT-Github" icon={DiGithubBadge} tooltipText="GitHub Platform" bgColor="bg-(--color-gold)/50" />
+                <IconWithTooltip id="TT-Bash" icon={SiGnubash} tooltipText="GNU Bash Shell" bgColor="bg-(--color-gold)/50" />
+                <IconWithTooltip id="TT-CMake" icon={SiCmake} tooltipText="CMake Build System" bgColor="bg-(--color-gold)/50" />
+                <IconWithTooltip id="TT-Make" icon={SiMake} tooltipText="Make Build Automation Tool" bgColor="bg-(--color-gold)/50" />
+                <IconWithTooltip id="TT-Docker" icon={DiDocker} tooltipText="Docker Containerization Platform" bgColor="bg-(--color-gold)/50" />
+                <IconWithTooltip id="TT-SQLite" icon={SiSqlite} tooltipText="PostgreSQL Database" bgColor="bg-(--color-gold)/50" />
+                <IconWithTooltip id="TT-MySQL" icon={DiMysql} tooltipText="MySQL Database" bgColor="bg-(--color-gold)/50" />
+                <IconWithTooltip id="TT-Hyprland" icon={SiHyprland} tooltipText="Hyprland Window Manager" bgColor="bg-(--color-gold)/50" />
+            </div>
 
-        <div className="flex flex-wrap gap-4 max-w-2xl rounded-2xl border-2 border-(--color-gold) p-2 bg-(--color-overlay)/80 mb-4" >
-          <IconWithTooltip id="TT-Linux" icon={DiLinux} tooltipText="Linux Operating System" bgColor="bg-(--color-gold)/50" />
-          <IconWithTooltip id="TT-Ubuntu" icon={SiUbuntu} tooltipText="Ubuntu Operating System" bgColor="bg-(--color-gold)/50" />
-          <IconWithTooltip id="TT-ArchLinux" icon={SiArchlinux} tooltipText="Arch Linux Operating System" bgColor="bg-(--color-gold)/50" />
-          <IconWithTooltip id="TT-Git" icon={DiGit} tooltipText="Git Version Control" bgColor="bg-(--color-gold)/50" />
-          <IconWithTooltip id="TT-Github" icon={DiGithubBadge} tooltipText="GitHub Platform" bgColor="bg-(--color-gold)/50" />
-          <IconWithTooltip id="TT-Bash" icon={SiGnubash} tooltipText="GNU Bash Shell" bgColor="bg-(--color-gold)/50" />
-          <IconWithTooltip id="TT-CMake" icon={SiCmake} tooltipText="CMake Build System" bgColor="bg-(--color-gold)/50" />
-          <IconWithTooltip id="TT-Make" icon={SiMake} tooltipText="Make Build Automation Tool" bgColor="bg-(--color-gold)/50" />
-          <IconWithTooltip id="TT-Docker" icon={DiDocker} tooltipText="Docker Containerization Platform" bgColor="bg-(--color-gold)/50" />
-          <IconWithTooltip id="TT-SQLite" icon={SiSqlite} tooltipText="PostgreSQL Database" bgColor="bg-(--color-gold)/50" />
-          <IconWithTooltip id="TT-MySQL" icon={DiMysql} tooltipText="MySQL Database" bgColor="bg-(--color-gold)/50" />
-          <IconWithTooltip id="TT-Hyprland" icon={SiHyprland} tooltipText="Hyprland Window Manager" bgColor="bg-(--color-gold)/50" />
+            <div className="flex flex-wrap gap-4 max-w-2xl rounded-2xl border-2 border-(--color-love) p-2 bg-(--color-overlay)/80 mb-4" >
+                <IconWithTooltip id="TT-RaspberryPi" icon={DiRasberryPi} tooltipText="Raspberry Pi Single-board Computer" bgColor="bg-(--color-love)/50" />
+                <IconWithTooltip id="TT-Arduino" icon={SiArduino} tooltipText="Arduino Microcontroller Platform" bgColor="bg-(--color-love)/50" />
+                <IconWithTooltip id="TT-Espressif" icon={SiEspressif} tooltipText="Espressif Systems IoT Platform" bgColor="bg-(--color-love)/50" />
+                <IconWithTooltip id="TT-ROS" icon={SiRos} tooltipText="Robot Operating System (ROS/ROS2)" bgColor="bg-(--color-love)/50" />
+            </div>
+
+            <div className="flex flex-wrap gap-4 max-w-2xl rounded-2xl border-2 border-(--color-pine) p-2 not-last:bg-(--color-overlay)/80 mb-4" >
+                <IconWithTooltip id="TT-React" icon={DiReact} tooltipText="React Framework" bgColor="bg-(--color-pine)/50" />
+                <IconWithTooltip id="TT-Nodejs" icon={DiNodejs} tooltipText="Node.js Runtime Environment" bgColor="bg-(--color-pine)/50"  />
+                <IconWithTooltip id="TT-Npm" icon={DiNpm} tooltipText="Node Package Manager" bgColor="bg-(--color-pine)/50" />
+                <IconWithTooltip id="TT-HTML5" icon={DiHtml5} tooltipText="HTML5 Markup Language" bgColor="bg-(--color-pine)/50" />
+                <IconWithTooltip id="TT-CSS3" icon={DiCss3} tooltipText="CSS3 Styling Language" bgColor="bg-(--color-pine)/50" />
+            </div>
+
+            <div className="flex flex-wrap gap-4 max-w-2xl rounded-2xl border-2 border-(--color-iris) p-2 bg-(--color-overlay)/80 mb-4" >
+                <IconWithTooltip id="TT-UML" icon={SiUml} tooltipText="Unified Modeling Language (UML)" bgColor="bg-(--color-iris)/50" />
+                <IconWithTooltip id="TT-Lucid" icon={SiLucid} tooltipText="Lucidchart Diagramming Tool" bgColor="bg-(--color-iris)/50" />
+                <IconWithTooltip id="TT-Figma" icon={SiFigma} tooltipText="Figma Design Tool" bgColor="bg-(--color-iris)/50" />
+                <IconWithTooltip id="TT-LaTeX" icon={SiLatex} tooltipText="LaTeX Document Preparation System" bgColor="bg-(--color-iris)/50" />
+                <IconWithTooltip id="TT-GoogleSheets" icon={SiGooglesheets} tooltipText="Google Sheets Spreadsheet Application" bgColor="bg-(--color-iris)/50" />
+                <IconWithTooltip id="TT-Blender" icon={SiBlender} tooltipText="Blender 3D Modeling Software" bgColor="bg-(--color-iris)/50" />
+                <IconWithTooltip id="TT-Gimp" icon={SiGimp} tooltipText="GIMP Image Editor" bgColor="bg-(--color-iris)/50" />
+            </div>
         </div>
-
-        <div className="flex flex-wrap gap-4 max-w-2xl rounded-2xl border-2 border-(--color-love) p-2 bg-(--color-overlay)/80 mb-4" >
-          <IconWithTooltip id="TT-RaspberryPi" icon={DiRasberryPi} tooltipText="Raspberry Pi Single-board Computer" bgColor="bg-(--color-love)/50" />
-          <IconWithTooltip id="TT-Arduino" icon={SiArduino} tooltipText="Arduino Microcontroller Platform" bgColor="bg-(--color-love)/50" />
-          <IconWithTooltip id="TT-Espressif" icon={SiEspressif} tooltipText="Espressif Systems IoT Platform" bgColor="bg-(--color-love)/50" />
-          <IconWithTooltip id="TT-ROS" icon={SiRos} tooltipText="Robot Operating System (ROS/ROS2)" bgColor="bg-(--color-love)/50" />
-        </div>
-
-        <div className="flex flex-wrap gap-4 max-w-2xl rounded-2xl border-2 border-(--color-pine) p-2 not-last:bg-(--color-overlay)/80 mb-4" >
-          <IconWithTooltip id="TT-React" icon={DiReact} tooltipText="React Framework" bgColor="bg-(--color-pine)/50" />
-          <IconWithTooltip id="TT-Nodejs" icon={DiNodejs} tooltipText="Node.js Runtime Environment" bgColor="bg-(--color-pine)/50"  />
-          <IconWithTooltip id="TT-Npm" icon={DiNpm} tooltipText="Node Package Manager" bgColor="bg-(--color-pine)/50" />
-          <IconWithTooltip id="TT-HTML5" icon={DiHtml5} tooltipText="HTML5 Markup Language" bgColor="bg-(--color-pine)/50" />
-          <IconWithTooltip id="TT-CSS3" icon={DiCss3} tooltipText="CSS3 Styling Language" bgColor="bg-(--color-pine)/50" />
-        </div>
-
-        <div className="flex flex-wrap gap-4 max-w-2xl rounded-2xl border-2 border-(--color-iris) p-2 bg-(--color-overlay)/80 mb-4" >
-          <IconWithTooltip id="TT-UML" icon={SiUml} tooltipText="Unified Modeling Language (UML)" bgColor="bg-(--color-iris)/50" />
-          <IconWithTooltip id="TT-Lucid" icon={SiLucid} tooltipText="Lucidchart Diagramming Tool" bgColor="bg-(--color-iris)/50" />
-          <IconWithTooltip id="TT-Figma" icon={SiFigma} tooltipText="Figma Design Tool" bgColor="bg-(--color-iris)/50" />
-          <IconWithTooltip id="TT-LaTeX" icon={SiLatex} tooltipText="LaTeX Document Preparation System" bgColor="bg-(--color-iris)/50" />
-          <IconWithTooltip id="TT-GoogleSheets" icon={SiGooglesheets} tooltipText="Google Sheets Spreadsheet Application" bgColor="bg-(--color-iris)/50" />
-          <IconWithTooltip id="TT-Blender" icon={SiBlender} tooltipText="Blender 3D Modeling Software" bgColor="bg-(--color-iris)/50" />
-          <IconWithTooltip id="TT-Gimp" icon={SiGimp} tooltipText="GIMP Image Editor" bgColor="bg-(--color-iris)/50" />
-        </div>
-
       </section>
     </section>
   )
